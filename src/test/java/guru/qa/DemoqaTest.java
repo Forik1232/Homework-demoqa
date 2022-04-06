bb3780d3a5c21d27f5ae3e60acdaf99d71073a1a
Daniil <9414354@mail.ru>
2022-04-06 11:54:24 +0300

    added .open space

---
+++ src/test/java/guru/qa/DemoqaTest.java
@@ -0,0 +1,24 @@
+package guru.qa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class DemoqaTest {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void practiceform(){
+        open("/automation-practice-form");
+    }
+}
+
+
